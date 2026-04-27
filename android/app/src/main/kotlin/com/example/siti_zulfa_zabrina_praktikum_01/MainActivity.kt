a243a339cb0fecaa3f9cd36d310ad1c7722c17a6
Zulfa-hub <zulfa.zabrina@gmail.com>
2026-04-27 21:33:40 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/siti_zulfa_zabrina_praktikum_01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.siti_zulfa_zabrina_praktikum_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+
+  void tambahCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hot Reload & Restart'),
+      ),
+      body: Center(
+        child: Text(
+          'Counter: $counter',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: tambahCounter,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
